96c2f020a3f7c0343ca4d728b220ad2d3762366f
Ganesh Raj Padhayay <ganeshraj020794@gmail.com>
2020-04-27 08:40:00 +0530

    content providers activity added

--- app/src/main/java/com/ganesh/androidfundamentals/MainActivity.kt
+++ app/src/main/java/com/ganesh/androidfundamentals/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.ganesh.androidfundamentals
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ganesh.androidfundamentals.contentproviders.ContentProvidersActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        buttonContentProviders.setOnClickListener {
+            startActivity(Intent(this, ContentProvidersActivity::class.java))
+        }
+
+        buttonActivityFragments.setOnClickListener {
+
+        }
+
+        buttonBroadcastReceivers.setOnClickListener {
+
+        }
+
+        buttonServices.setOnClickListener {
+
+        }
     }
 }
---
+++ app/src/main/java/com/ganesh/androidfundamentals/contentproviders/ContentProvidersActivity.kt
@@ -0,0 +1,13 @@
+package com.ganesh.androidfundamentals.contentproviders
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ganesh.androidfundamentals.R
+
+class ContentProvidersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_content_providers)
+    }
+}
