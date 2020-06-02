a78a85b94cfbb3993822d3a121d276ec10262180
Ganesh Raj Padhayay <ganeshraj020794@gmail.com>
2020-06-02 17:19:26 +0530

    leetcode june updated

---
+++ algorithms/src/main/java/com/ganesh/leetcode/june/Problem2.java
@@ -0,0 +1,20 @@
+package com.ganesh.leetcode.june;
+
+public class Problem2 {
+
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+
