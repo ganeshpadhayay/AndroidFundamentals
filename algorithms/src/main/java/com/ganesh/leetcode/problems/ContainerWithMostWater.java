1eaa247ceefee952916eae2a264d6e7d89873dd6
Ganesh Raj Padhayay <ganeshraj020794@gmail.com>
2020-06-04 18:59:58 +0530

    leetcode problems added

---
+++ algorithms/src/main/java/com/ganesh/leetcode/problems/ContainerWithMostWater.java
@@ -0,0 +1,22 @@
+package com.ganesh.leetcode.problems;
+
+public class ContainerWithMostWater {
+
+    public static void main(String[] args) {
+        System.out.println(new ContainerWithMostWater().maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7}));
+    }
+
+    public int maxArea(int[] height) {
+        int low = 0, high = height.length - 1;
+        int maxArea = Integer.MIN_VALUE;
+        while (low < high) {
+            maxArea = Math.max(maxArea, Math.min(height[low], height[high]) * (high - low));
+            if (height[low] < height[high]) {
+                low++;
+            } else {
+                high--;
+            }
+        }
+        return maxArea;
+    }
+}
---
+++ algorithms/src/main/java/com/ganesh/leetcode/problems/ProductOfArrayExceptSelf.java
@@ -0,0 +1,49 @@
+package com.ganesh.leetcode.problems;
+
+public class ProductOfArrayExceptSelf {
+
+    public static void main(String[] args) {
+        int[] answer = new ProductOfArrayExceptSelf().productExceptSelf2(new int[]{1, 2, 3, 4});
+        for (int i = 0; i < answer.length; i++) {
+            System.out.println(answer[i] + " ");
+        }
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] productArray = new int[nums.length];
+        int[] leftProductArray = new int[nums.length];
+        int[] rightProductArray = new int[nums.length];
+
+        leftProductArray[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            leftProductArray[i] = leftProductArray[i - 1] * nums[i - 1];
+        }
+
+        rightProductArray[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            rightProductArray[i] = rightProductArray[i + 1] * nums[i + 1];
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            productArray[i] = leftProductArray[i] * rightProductArray[i];
+        }
+        return productArray;
+    }
+
+    public int[] productExceptSelf2(int[] nums) {
+        int[] productArray = new int[nums.length];
+
+        productArray[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            productArray[i] = productArray[i - 1] * nums[i - 1];
+        }
+
+        int R = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            productArray[i] = productArray[i] * R;
+            R = R * nums[i];
+        }
+
+        return productArray;
+    }
+}
