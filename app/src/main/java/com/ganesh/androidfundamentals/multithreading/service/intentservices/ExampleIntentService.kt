db37bc74b1a6a22ce04d91568d7ccd74c1005a62
Ganesh Raj Padhayay <ganeshraj020794@gmail.com>
2020-06-04 15:59:07 +0530

    intent service added

---
+++ app/src/main/java/com/ganesh/androidfundamentals/multithreading/service/intentservices/ExampleIntentService.kt
@@ -0,0 +1,77 @@
+package com.ganesh.androidfundamentals.multithreading.service.intentservices
+
+import android.app.IntentService
+import android.app.Notification
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.PowerManager
+import android.os.SystemClock
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.ganesh.androidfundamentals.App.Companion.CHANNEL_ID
+import com.ganesh.androidfundamentals.R
+
+/***
+ * As you can notice, here we do not need to override onBind() method as IntentService does that for us
+ * Also, no need to handle onStartCommand() method here
+ */
+class ExampleIntentService : IntentService("ExampleIntentService") {
+
+    //alternate of Java's static final constant
+    companion object {
+        private const val TAG: String = "ExampleIntentService"
+    }
+
+    private lateinit var wakeLock: PowerManager.WakeLock
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d(TAG, "onCreate")
+
+        //acquire wake lock
+        var powerManager: PowerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ExampleApp:WakeLock")
+        wakeLock.acquire(60* 1000)
+        Log.d(TAG, "wakelock acquired")
+
+        //create notification one time here
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            var notification: Notification = NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Example IntentService")
+                .setContentText("Running...")
+                .setSmallIcon(R.drawable.ic_android)
+                .build()
+
+            //this will make sure that the service is a foreground service
+            startForeground(1, notification)
+        }
+
+    }
+
+    //this is the substitute of return type(STICKY, NON_STICKY from services)
+    init {
+        setIntentRedelivery(false)
+    }
+
+    //this method gets executed on the background thread
+    //intents will be passed sequentially if needs to run those intents in parallel we can extend from normal service and spawn multiple threads
+    //for each intent
+    override fun onHandleIntent(intent: Intent?) {
+        Log.d(TAG, "onHandleIntent")
+
+        val input: String? = intent?.getStringExtra("inputExtra")
+
+        for (i in 1..10) {
+            Log.d(TAG, "$input - $i")
+            SystemClock.sleep(1000)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        wakeLock.release()
+        Log.d(TAG, "wakelock released")
+    }
+
+}
--- app/src/main/java/com/ganesh/androidfundamentals/multithreading/service/intentservices/SampleIntentServiceActivity.kt
+++ app/src/main/java/com/ganesh/androidfundamentals/multithreading/service/intentservices/SampleIntentServiceActivity.kt
@@ -1,18 +1,49 @@
 package com.ganesh.androidfundamentals.multithreading.service.intentservices
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.ganesh.androidfundamentals.R
+import com.ganesh.androidfundamentals.multithreading.service.services.ExampleService
+import kotlinx.android.synthetic.main.activity_sample_foreground_service.*
 
+/***
+ * IntentService is a base class for Services that handle asynchronous requests (expressed as Intents) on demand.
+ * Clients send requests through Context.startService(Intent) calls; the service is started as needed, handles each Intent in turn using
+ * a worker thread, and stops itself when it runs out of work.
+ *
+ * Also we do need to handle the killing of this service as intent service does ghat for us, it terminated when it finishes the execution
+
+This "work queue processor" pattern is commonly used to offload tasks from an application's main thread. The IntentService class exists to
+simplify this pattern and take care of the mechanics. To use it, extend IntentService and implement onHandleIntent(android.content.Intent).
+IntentService will receive the Intents, launch a worker thread, and stop the service as appropriate.
+
+All requests are handled on a single worker thread -- they may take as long as necessary (and will not block the application's main loop),
+but only one request will be processed at a time.
+ */
 class SampleIntentServiceActivity : AppCompatActivity() {
 
     //alternate of Java's static final constant
     companion object {
-        const val TAG: String = "MultiThreadingActivity"
+        const val TAG: String = "SampleIntentServiceActivity"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sample_intent_service)
     }
+
+    fun startIntentService(view: View) {
+        val input: String = edit_text_input.text.toString()
+        val serviceIntent: Intent = Intent(this, ExampleIntentService::class.java)
+        serviceIntent.putExtra("inputExtra", input)
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
 }
+
+
+//so, we can either use JobIntentService for API 26 above but the problem with the job is it can be deferred but a service(intentService) is started
+//immediately so we can use intent service for both API level less than 26 and above also using startForegroundService()
