ab1b34cbad443f098afbb4ae4a002335c369149c
Ganesh Raj Padhayay <ganeshraj020794@gmail.com>
2020-06-06 22:05:13 +0530

    codechef updated

--- algorithms/src/main/java/com/ganesh/codechef/JuneProblem4.java
+++ algorithms/src/main/java/com/ganesh/codechef/JuneProblem4.java
@@ -1,5 +1,21 @@
 package com.ganesh.codechef;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class JuneProblem4 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            long TS = Long.parseLong(br.readLine());
+            while (TS % 2 == 0) {
+                TS /= 2;
+            }
+            System.out.println(TS / 2);
+        }
+    }
 
 }
