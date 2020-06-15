66404e41f6d46c725f117a475e2f0ad7fed4d1f9
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-06-15 17:27:18 +0530

    adding test commmit 2 with ssh key on github

--- algorithms/src/main/java/com/ganesh/Test.java
+++ algorithms/src/main/java/com/ganesh/Test.java
@@ -3,5 +3,6 @@ package com.ganesh;
 public class Test {
     public static void main(String[] args){
         System.out.println("Hello testing git from a different system");
+        System.out.println("Making another commit form system 2 without ssh key on github");
     }
 }
