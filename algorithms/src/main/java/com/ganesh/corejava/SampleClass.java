db014e7a5285b191685cf4706b1ecbb3d1a336a7
Ganesh Raj Padhayay <ganeshraj020794@gmail.com>
2020-06-25 17:00:46 +0530

    test2

--- algorithms/src/main/java/com/ganesh/corejava/SampleClass.java
+++ algorithms/src/main/java/com/ganesh/corejava/SampleClass.java
@@ -3,5 +3,6 @@ package com.ganesh.corejava;
 public class SampleClass {
     public static void main(String[] args) {
         System.out.println("Testing");
+        System.out.println("Testing again");
     }
 }
