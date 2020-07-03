2b620d8eb94b5807ae14e4ce7f8ca1fda25e4395
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-07-03 10:48:44 +0530

    work manager code added with viewbinding true

--- app/src/main/java/com/ganesh/androidfundamentals/MyApplication.kt
+++ app/src/main/java/com/ganesh/androidfundamentals/MyApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.os.Build
+import timber.log.Timber
 
 class MyApplication : Application() {
 
@@ -13,13 +14,22 @@ class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        createNotificationChannel();
+        createNotificationChannel()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     private fun createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val notificationChannel = NotificationChannel(CHANNEL_ID, "Example Service Channel", NotificationManager.IMPORTANCE_DEFAULT)
-            val notificationManager = getSystemService(NotificationManager::class.java) as NotificationManager
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                "Example Service Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            val notificationManager =
+                getSystemService(NotificationManager::class.java) as NotificationManager
             notificationManager.createNotificationChannel(notificationChannel)
         }
     }
