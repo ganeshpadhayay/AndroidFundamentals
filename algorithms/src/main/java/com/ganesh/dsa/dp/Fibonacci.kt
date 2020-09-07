787469ac64b401009aa7767bc762e2a6eb265bf9
Ganesh Padhayay <ganeshraj020794@gmail.com>
2020-09-07 15:28:39 +0530

    added lcs problem

--- algorithms/src/main/java/com/ganesh/dsa/dp/Fibonacci.kt
+++ algorithms/src/main/java/com/ganesh/dsa/dp/Fibonacci.kt
@@ -15,6 +15,10 @@ object Fibonacci {
         println(fibonacciTabulation(num))
     }
 
+    /**
+     * here recursion call is involved but it is easy to fill the memo array
+     * it is slower
+     */
     private fun fibonacciMemoization(num: Int): Int {
         if (memo[num] == 0) {
             val res: Int = if (num == 0 || num == 1) {
@@ -27,6 +31,10 @@ object Fibonacci {
         return memo[num]
     }
 
+    /**
+     *here no recursion call is involved but table is difficult to fill for some questions
+     * it is faster
+     */
     private fun fibonacciTabulation(n: Int): Int {
         val f = IntArray(n + 1)
         f[0] = 0
---
+++ algorithms/src/main/java/com/ganesh/dsa/dp/LongestCommonSubsequence.kt
@@ -0,0 +1,50 @@
+package com.ganesh.dsa.dp
+
+import java.util.*
+import kotlin.math.max
+
+object LongestCommonSubsequence {
+    private lateinit var memo: Array<IntArray>
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val s1 = "AXYZ"
+        val s2 = "BAZ"
+        val n = 4
+        val m = 3
+        memo = Array(n + 1) { IntArray(m + 1) }
+        for (i in memo) {
+            Arrays.fill(i, -1)
+        }
+        println(lcsMemoization(s1, s2, n, m))
+
+        //tabulation
+        println(lcsTabulation(s1, s2))
+    }
+
+    private fun lcsMemoization(s1: String, s2: String, n: Int, m: Int): Int {
+        if (memo[n][m] != -1) return memo[n][m]
+        if (n == 0 || m == 0)
+            memo[n][m] = 0
+        else {
+            if (s1[n - 1] == s2[m - 1])
+                memo[n][m] = 1 + lcsMemoization(s1, s2, n - 1, m - 1)
+            else memo[n][m] = max(lcsMemoization(s1, s2, n - 1, m), lcsMemoization(s1, s2, n, m - 1))
+        }
+        return memo[n][m]
+    }
+
+    private fun lcsTabulation(s1: String, s2: String): Int {
+        val m = s1.length
+        val n = s2.length
+        val dp = Array(m + 1) { IntArray(n + 1) }
+        for (i in 1..m) {
+            for (j in 1..n) {
+                if (s1[i - 1] == s2[j - 1])
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+        return dp[m][n]
+    }
+}
